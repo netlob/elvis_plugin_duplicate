9302390606afdc80dd7764e52d75d631746c7347
Sjoerd Bolten <sjoerdabolten@gmail.com>
2020-05-14 11:22:03 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,7 @@ class Server {
                         if (hit.id == req.body.assetId) return false;
                         return hit.metadata.firstExtractedChecksum == newChecksum;
                     });
-                    await this.apiManager.update(req.body.assetId, JSON.stringify({ cf_duplicate: (duplicate != undefined) }));     
+                    await this.apiManager.update(req.body.assetId, JSON.stringify({ cf_duplicate: !!duplicate }));     
                 }
             } catch (e) {
                 console.error(e);
